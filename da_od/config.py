8959775d15ecc6e26e706d89455ad735d1e1e6dd
Maryam Mohebbi <maryam.mohebbi.2@gmail.com>
2024-02-21 15:46:26 +0100

    removed extra files

--- da_od/config.py
+++ da_od/config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-data_path = Path(__file__).parent / "data"
+data_path = Path(__file__).parent.parent / "data"
 
 test_img = Path(__file__).parent.parent / "test-imgs"
 output_img = Path(__file__).parent.parent / "output-imgs"
@@ -11,3 +11,8 @@ annotation_path = data_path / "gtFine_trainvaltest/gtFine"
 image_path = data_path / "leftImg8bit_trainvaltest/leftImg8bit"
 
 sam_weights = Path(__file__).parent / "model"
+
+
+class_names = data_path / "class_names"
+sam_weights = data_path / "sam_weights"
+yolo_weights = data_path / "yolo_weights"
