292693961331ea2a354eed39f5336509a731dffe
Maryam Mohebbi <maryam.mohebbi.2@gmail.com>
2024-02-23 15:32:34 +0100

    __init__

--- da_od/model/__init__.py
+++ da_od/model/__init__.py
@@ -1,3 +1,10 @@
 from .depth import DepthAnythingEstimator, MiDaSEstimator
+from .monodepth import DepthDecoder, ResnetEncoder, download_model_if_doesnt_exist
 
-__all__ = ["DepthAnythingEstimator", "MiDaSEstimator"]
+__all__ = [
+    "DepthAnythingEstimator",
+    "MiDaSEstimator",
+    "ResnetEncoder",
+    "DepthDecoder",
+    "download_model_if_doesnt_exist",
+]
