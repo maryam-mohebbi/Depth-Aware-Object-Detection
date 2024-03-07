d388a685f739f9d0f0bfcdc3fff977f2c17e7cde
Maryam Mohebbi <maryam.mohebbi.2@gmail.com>
2024-03-07 13:36:53 +0100

    `SamModelManager` and `VisualizationTools` refactoring

--- da_od/model/seg_sam.py
+++ da_od/model/seg_sam.py
@@ -5,66 +5,70 @@ import torch
 from da_od.segment_anything import sam_model_registry
 
 
-def get_model(CHECKPOINT_PATH):
-    DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    MODEL_TYPE = "vit_h"
+class SamModelManager:
+    def __init__(self, checkpoint_path, model_type="vit_h"):
+        self.checkpoint_path = checkpoint_path
+        self.model_type = model_type
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.model = self.load_model()
 
-    sam = sam_model_registry[MODEL_TYPE](checkpoint=CHECKPOINT_PATH)
-    sam.to(device=DEVICE)
-
-    return sam
-
-
-def show_mask(mask, ax, random_color=False):
-    if random_color:
-        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
-    else:
-        color = np.array([30 / 255, 144 / 255, 255 / 255, 0.6])
-    h, w = mask.shape[-2:]
-    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
-    ax.imshow(mask_image)
+    def load_model(self):
+        sam = sam_model_registry[self.model_type](checkpoint=self.checkpoint_path)
+        sam.to(device=self.device)
+        return sam
 
 
-def show_points(coords, labels, ax, marker_size=365):
-    pos_points = coords[labels == 1]
-    neg_points = coords[labels == 0]
-    ax.scatter(
-        pos_points[:, 0],
-        pos_points[:, 1],
-        color="green",
-        marker="*",
-        s=marker_size,
-        edgecolor="white",
-        linewidth=1.25,
-    )
-    ax.scatter(
-        neg_points[:, 0],
-        neg_points[:, 1],
-        color="red",
-        maker="*",
-        s=marker_size,
-        edgecolor="white",
-        linewidth=1.25,
-    )
+class VisualizationTools:
+    @staticmethod
+    def show_mask(mask, ax, random_color=False):
+        if random_color:
+            color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+        else:
+            color = np.array([30 / 255, 144 / 255, 255 / 255, 0.6])
+        h, w = mask.shape[-2:]
+        mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+        ax.imshow(mask_image)
 
+    @staticmethod
+    def show_points(coords, labels, ax, marker_size=365):
+        pos_points = coords[labels == 1]
+        neg_points = coords[labels == 0]
+        ax.scatter(
+            pos_points[:, 0],
+            pos_points[:, 1],
+            color="green",
+            marker="*",
+            s=marker_size,
+            edgecolor="white",
+            linewidth=1.25,
+        )
+        ax.scatter(
+            neg_points[:, 0],
+            neg_points[:, 1],
+            color="red",
+            marker="*",
+            s=marker_size,
+            edgecolor="white",
+            linewidth=1.25,
+        )
 
-def show_box(box, ax):
-    x0, y0 = box[0], box[1]
-    w, h = box[2] - box[0], box[3] - box[1]
-    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor="green", facecolor=(0, 0, 0, 0), lw=2))
+    @staticmethod
+    def show_box(box, ax):
+        x0, y0 = box[0], box[1]
+        w, h = box[2] - box[0], box[3] - box[1]
+        ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor="green", facecolor=(0, 0, 0, 0), lw=2))
 
+    @staticmethod
+    def show_anns(anns):
+        if len(anns) == 0:
+            return
+        sorted_anns = sorted(anns, key=(lambda x: x["area"]), reverse=True)
+        ax = plt.gca()
+        ax.set_autoscale_on(False)
 
-def show_anns(anns):
-    if len(anns) == 0:
-        return
-    sorted_anns = sorted(anns, key=(lambda x: x["area"]), reverse=True)
-    ax = plt.gca()
-    ax.set_autoscale_on(False)
-
-    img = np.ones((sorted_anns[0]["segmentation"].shape[0], sorted_anns[0]["segmentation"].shape[1], 4))
-    img[:, :, 3] = 0
-    for ann in sorted_anns:
-        m = ann["segmentation"]
-        color_mask = np.concatenate([np.random.random(3), [0.35]])
-        img[m] = color_mask
-    # ax.imshow(img)
+        img = np.ones((sorted_anns[0]["segmentation"].shape[0], sorted_anns[0]["segmentation"].shape[1], 4))
+        img[:, :, 3] = 0
+        for ann in sorted_anns:
+            m = ann["segmentation"]
+            color_mask = np.concatenate([np.random.random(3), [0.35]])
+            img[m] = color_mask
