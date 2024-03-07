f83a5a7714b3f33cf3b6b406b784039958388eb5
Maryam Mohebbi <maryam.mohebbi.2@gmail.com>
2024-03-07 12:40:02 +0100

    `ObjectDetector` refactoring

--- da_od/model/yolo_nas.py
+++ da_od/model/yolo_nas.py
@@ -1,37 +1,29 @@
-from typing import Union
-
 import cv2
 import numpy as np
 from super_gradients.training import models
 
 
-def get_prediction_info(predictions):
-    class_names = predictions.class_names
-    labels = predictions.prediction.labels
-    confidence = predictions.prediction.confidence
-    bboxes = predictions.prediction.bboxes_xyxy
-    return bboxes, confidence, labels, class_names
+class ObjectDetector:
+    def __init__(self, model_name="yolo_nas_l", pretrained_weights="coco", conf_threshold=0.25):
+        self.model = models.get(model_name, pretrained_weights=pretrained_weights)
+        self.conf_threshold = conf_threshold
 
+    def get_prediction_info(self, predictions):
+        class_names = predictions.class_names
+        labels = predictions.prediction.labels
+        confidence = predictions.prediction.confidence
+        bboxes = predictions.prediction.bboxes_xyxy
+        return bboxes, confidence, labels, class_names
 
-def get_object_detection(image_input: Union[str, np.ndarray]):
-    # Check if the input is a string (path) or an image array
-    if isinstance(image_input, str):
-        image = cv2.imread(image_input)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    elif isinstance(image_input, np.ndarray):
-        image = cv2.cvtColor(image_input, cv2.COLOR_BGR2RGB)
-    else:
-        raise ValueError("Image input must be a file path (string) or an image array (numpy.ndarray)")
+    def detect_objects(self, image_input):
+        if isinstance(image_input, str):
+            image = cv2.imread(image_input)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        elif isinstance(image_input, np.ndarray):
+            image = cv2.cvtColor(image_input, cv2.COLOR_BGR2RGB)
+        else:
+            raise ValueError("Image input must be a file path (string) or an image array (numpy.ndarray)")
 
-    model = models.get("yolo_nas_l", pretrained_weights="coco")
-
-    conf_threshold = 0.25
-
-    if isinstance(image_input, np.ndarray):
-        detection_pred = model.predict(image, conf=conf_threshold)  # Adjusted to use the image array
-    else:
-        detection_pred = model.predict(str(image_input), conf=conf_threshold)
-
-    bboxes, confidence, labels, class_names = get_prediction_info(detection_pred)
-
-    return bboxes, confidence, labels, class_names, image
+        detection_pred = self.model.predict(image, conf=self.conf_threshold)
+        bboxes, confidence, labels, class_names = self.get_prediction_info(detection_pred)
+        return bboxes, confidence, labels, class_names, image
